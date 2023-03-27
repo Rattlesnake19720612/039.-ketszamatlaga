7d5d89ceb134895d2da83bc255fd623d28785904
Dömötör Gyula <grattlesnake@gmail.com>
2023-03-27 19:20:49 +0200

    Két szám átlaga

---
+++ 039. ketszamatlaga/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _039.ketszamatlaga
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kérek egy számot!");
+            int szam1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Kérek még egy számot!");
+            int szam2 = Convert.ToInt32(Console.ReadLine());
+
+            int atlag = (szam1 + szam2) / 2;
+
+            Console.WriteLine("A számok átlaga: " + atlag);
+
+            Console.ReadKey();
+        }
+    }
+}
