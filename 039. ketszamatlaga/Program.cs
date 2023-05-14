947df64e56d9dbdc1c546d83e580eb2eeca86eb1
DGYKKSZKI <124397426+Rattlesnake19720612@users.noreply.github.com>
2023-05-14 23:10:11 +0200

    Update Program.cs

--- 039. ketszamatlaga/Program.cs
+++ 039. ketszamatlaga/Program.cs
@@ -23,3 +23,7 @@ namespace _039.ketszamatlaga
         }
     }
 }
+
+/*Két számot kér be a felhasználótól, majd kiszámolja azok átlagát és kiírja az eredményt a konzolra. A Main() függvény vezérli a program futását. Első lépésként kiírja a "Kérek egy számot!" szöveget a konzolra, majd beolvassa a felhasználó által megadott számot a szam1 változóba. Majd a felhasználótól kapott, stringet (szöveges változót) a ConvertToInt32 metódussal átalakítja integer (int) egész számos változóvá, hogy matematikai műveleteket tudjunk vele végezni. Ezt követően ugyanezt teszi a "Kérek még egy számot!" szöveggel és a második számmal (szam2) is.
+
+Az atlag változóba beállítjuk az szam1 és szam2 összege osztva 2-vel kapott átlagát. Végül a program kiírja az átlagot a "A számok átlaga:" szöveggel együtt a konzolra. A Console.ReadKey() függvény segítségével várunk egy billentyűlenyomást, mielőtt a program véget érne és kilép.*/
